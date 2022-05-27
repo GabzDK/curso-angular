277824243df0cc45fc603d4ac49f930cb1631598
Pedro <pedro.henrique@soundcloud-externals.com>
2022-05-27 15:32:34 -0300

    fix getid

--- rotas/src/app/app.component.ts
+++ rotas/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rotas';
+  private id: string;
+
+  constructor(private router: Router) {
+  }
+
+  getIdCurso() {
+    this.id = (document.getElementById("idCurso") as HTMLInputElement).value
+    this.router.navigate(['/curso/',this.id])
+  }
 }
